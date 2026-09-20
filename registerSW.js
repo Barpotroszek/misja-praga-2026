1111834388f9ad15419643ff3825d089d0960539
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2026-09-20 14:01:32 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/misja-praga-2026/sw.js', { scope: '/misja-praga-2026/' })})}
